ccd374bee969357c43f89d058233f9e621283c64
tim <schuberttim237@gmail.com>
2024-09-13 11:59:16 +0200

    newTodoItem moved to store and works now

--- src/components/my-component/todo list/todo-list.tsx
+++ src/components/my-component/todo list/todo-list.tsx
@@ -1,6 +1,6 @@
-import { Component, h } from '@stencil/core';
-import { store } from '../../../reduxStore/store';
-import { addTodo, updateNewTaskText, toggleTodo } from '../../../reduxStore/store';
+import { Component, h, State } from '@stencil/core';
+import { store, addTodo } from '../../../reduxStore/store';
+import { updateNewTaskText, toggleTodo } from '../../../reduxStore/store';
 import { Task } from './task';
 
 @Component({
@@ -9,17 +9,27 @@ import { Task } from './task';
   shadow: true,
 })
 export class TodoList {
+  @State() tasks: Task[] = [];
+  @State() newTaskText: string = '';
+
+  componentWillLoad() {
+    store.subscribe(() => {
+      const state = store.getState();
+      this.tasks = [...state.todos].sort((a, b) => Number(a.isChecked) - Number(b.isChecked));
+      this.newTaskText = state.newTaskText;
+    });
+  }
 
   handleInputChange(event: Event) {
-    console.log("InputChange", event);
+    console.log('InputChange', event);
     const target = event.target as HTMLInputElement;
     store.dispatch(updateNewTaskText(target.value));
   }
 
   handleFormSubmit(event: Event) {
-    console.log("FormSubmit", event);
+    console.log('FormSubmit', event);
     event.preventDefault();
-    if (store.getState().newTaskText  !== '') {
+    if (store.getState().newTaskText !== '') {
       const task = new Task(store.getState().newTaskText);
       store.dispatch(addTodo(task));
       store.dispatch(store.dispatch(updateNewTaskText('')));
