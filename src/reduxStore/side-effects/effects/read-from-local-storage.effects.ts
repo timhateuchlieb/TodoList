6af2cf5ab2af2bd7ad6ac106edce399de10a3815
tim <schuberttim237@gmail.com>
2025-01-21 12:01:37 +0100

    Fix loading todos from local storage

--- src/reduxStore/side-effects/effects/read-from-local-storage.effects.ts
+++ src/reduxStore/side-effects/effects/read-from-local-storage.effects.ts
@@ -3,11 +3,11 @@ import { updateAccordingToLocalStorageError } from '../../actions/actions';
 import { updateAccordingToLocalStorageSuccess } from '../../actions/actions';
 
 export function* readFromLocalStorageEffects() {
+  console.log('readFromLocalStorageEffects');
   try {
     const todoState = localStorage.getItem('todoState');
-
-    yield put(updateAccordingToLocalStorageSuccess( JSON.parse(todoState)));
-
+    const darkMode = localStorage.getItem('darkMode');
+    yield put(updateAccordingToLocalStorageSuccess( {todos: JSON.parse(todoState), darkMode: darkMode === 'true'}));
   } catch (error) {
     yield put(updateAccordingToLocalStorageError());
   }
