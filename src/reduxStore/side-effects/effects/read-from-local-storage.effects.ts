a7a2aa2c024965e308b25471cea1ada3a10d4483
tim <schuberttim237@gmail.com>
2025-01-22 09:18:07 +0100

    Remove redundant dark mode class check from local storage effects 🤡

--- src/reduxStore/side-effects/effects/read-from-local-storage.effects.ts
+++ src/reduxStore/side-effects/effects/read-from-local-storage.effects.ts
@@ -6,11 +6,6 @@ export function* readFromLocalStorageEffects() {
   try {
     const todoState = localStorage.getItem('todoState');
     const darkMode = localStorage.getItem('darkMode') === 'true';
-    const isDarkModeClass = document.documentElement.classList.contains('darkMode');
-
-    if (darkMode !== isDarkModeClass) {
-      document.documentElement.classList.toggle('darkMode', darkMode);
-    }
 
     yield put(updateAccordingToLocalStorageSuccess( {todos: JSON.parse(todoState), darkMode: darkMode}));
   } catch (error) {
